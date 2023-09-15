441c7ec748bf6a57ffd50bb4eddcfcee12c5293d
student <aekang12@gmail.com>
2023-09-15 18:08:36 -0400

    got rid of if statement, added comments

--- annkang_student_turtle.cpp
+++ annkang_student_turtle.cpp
@@ -12,56 +12,60 @@
  */
 
 #include "student.h"
+#include <unistd.h>
 
 // Ignore this line until project 5
 turtleMove studentTurtleStep(bool bumped) {return MOVE;}
 
 // OK TO MODIFY BELOW THIS LINE
 
-#define TIMEOUT 40    // bigger number slows down simulation so you can see what's happening
-float w, cs;
+int move;
 		 
 // this procedure takes the current turtle position and orientation and returns
 // true=submit changes, false=do not submit changes
 // Ground rule -- you are only allowed to call the helper functions "bumped(..)" and "atend(..)",
 // and NO other turtle methods or maze methods (no peeking at the maze!)
-bool studentMoveTurtle(QPointF& pos_, int& nw_or) {
-    //ROS_INFO("Turtle update Called  w=%f", w);
-    bool bp = false;
-
+bool studentMoveTurtle(QPointF& pos_, int& dir) {
     if (atend(pos_.x(), pos_.y())) {
 	return false;
     }
 
-    if(w == 0) {
-	if (nw_or==0) {
-	    bp = bumped(pos_.x(),pos_.y(),pos_.x(),pos_.y()+1);
-	} else if (nw_or == 1) {
-	    bp = bumped(pos_.x(),pos_.y(),pos_.x()+1,pos_.y());
-	} else if (nw_or == 2) {
-	    bp = bumped(pos_.x()+1,pos_.y(),pos_.x()+1,pos_.y()+1);
-	} else {
-	    bp = bumped(pos_.x(),pos_.y()+1,pos_.x()+1,pos_.y()+1);
-	}
-	
-	if (cs == 2) {
-	    nw_or = (nw_or+3) % 4;
-	    cs = 1;
-	} else if (bp) {
-	    nw_or = (nw_or + 1) % 4; 
-	    cs = 0;
-	} else {
-	    cs = 2;
-     if (nw_or == 1) pos_.setY(pos_.y() - 1); 
-     if (nw_or == 2) pos_.setX(pos_.x() + 1);
-     if (nw_or == 3) pos_.setY(pos_.y() + 1);
-     if (nw_or == 0) pos_.setX(pos_.x() - 1);
-	}	
-	 //ROS_INFO("Orientation=%f  STATE=%f", nw_or, cs);
-	w = TIMEOUT;
+    sleep(1);
+    
+    // vectors used for wall checking, indexed by turtle direction
+    int wall_coords[4][4] = {{0,0,0,1}, {0,0,1,0}, {1,0,1,1}, {0,1,1,1}};
+
+    // if turtle went straight, turn left
+    if (move == 2) {
+	dir = (dir + 3) % 4;
+	move = 1;
 	return true;
+    }
+ 
+    // check if there is a wall in front of the turtle
+    bool wall = bumped(pos_.x() + wall_coords[dir][0], \
+    pos_.y() + wall_coords[dir][1], \
+    pos_.x() + wall_coords[dir][2],pos_.y() + wall_coords[dir][3]);
+
+    // turn right	
+    if (wall) {
+	dir = (dir + 1) % 4; 
+	move = 0;
+	return true;
+    }
+
+    // go straight 
+    move = 2;
+    if (dir == 1) {
+	pos_.setY(pos_.y() - 1);
+    } else if (dir == 2) {
+	pos_.setX(pos_.x() + 1);
+    } else if (dir == 3) {
+	pos_.setY(pos_.y() + 1);
     } else {
-	w -= 1;
- }
- return false;
+        pos_.setX(pos_.x() - 1);
+    }
+    
+    ROS_INFO("Orientation=%f  STATE=%f", dir, move);
+    return true;
 }
