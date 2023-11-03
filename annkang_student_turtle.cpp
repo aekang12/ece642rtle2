1ce69a37fce9ff89be178f41e8fc7de94f9e1a1e
student <aekang12@gmail.com>
2023-11-03 13:40:48 -0400

    working project 7 code

--- annkang_student_turtle.cpp
+++ annkang_student_turtle.cpp
@@ -21,13 +21,13 @@ static struct Tracker{
     uint8_t array[23][23] = {};
     uint8_t x = 11; 
     uint8_t y = 11; 
-    
-    // constructor for initialization 
-    Tracker() {
-	array[11][11] = 1; 
-    }
 } tracker;
 static int memory[3] = {0}; 
+typedef struct coordinates coordinates; 
+struct coordinates {
+    uint8_t x;
+    uint8_t y;
+};
 
 // clear the turtle's memory 
 void clear_memory() {
@@ -36,7 +36,7 @@ void clear_memory() {
     memory[(int)Right] = 0; 
 }
 
-// track number of visits to each cell in the turtle's local array
+// update turtle's local (x,y) position
 void update_pos(int& direction) {
     switch ((Directions)direction) {
 	case North:
@@ -54,33 +54,36 @@ void update_pos(int& direction) {
 	default:
 	    ROS_ERROR("Invalid direction!"); 
     }
+    tracker.array[tracker.x][tracker.y] += 1;
+    mazeVisits(tracker.array[tracker.x][tracker.y]);
     return; 
 }
 
-// gets turtle's relative coordinates of the cell direclty in front of it 
-uint8_t* get_pos_ahead(int& direction) {
-    uint8_t coords[2]; 
+// gets coordinates of the cell direclty in front of it 
+coordinates get_pos_ahead(int& direction) {
+    coordinates c; 
     switch ((Directions)direction) {
 	case North:
-	    coords[0] = tracker.x;
-	    coords[1] = tracker.y + 1;
-	    return coords;
+	    c.x = tracker.x;
+	    c.y = tracker.y + 1;
+	    break;
 	case East:
-	    coords[0] = tracker.x+1;
-	    coords[1] = tracker.y; 
-	    return coords;
+	    c.x = tracker.x+1;
+	    c.y = tracker.y; 
+	    break;
 	case South:
-	    coords[0] = tracker.x;
-	    coords[1] = tracker.y-1; 
-	    return coords;
+	    c.x = tracker.x;
+	    c.y = tracker.y-1; 
+	    break;
 	case West:
-	    coords[0] = tracker.x-1;
-	    coords[1] = tracker.y; 
-	    return coords;
+	    c.x = tracker.x-1;
+	    c.y = tracker.y; 
+	    break;
 	default:
 	    ROS_ERROR("Invalid direction!"); 
-	    return coords; 
+	    break;
     }
+    return c;
 }
 
 // updates the turtle's memory of what's direclty in front of it 
@@ -89,8 +92,8 @@ int look_ahead(bool bumped, int& direction) {
     if (bumped) {
 	return wall_val; 
     } else {
-	uint8_t* coords = get_pos_ahead(direction); 
-    	return tracker.array[coords[0]][coords[1]];
+	coordinates c = get_pos_ahead(direction); 
+    	return tracker.array[c.x][c.y];
     }
 }
 
@@ -142,8 +145,7 @@ Moves studentTurtleStep(bool bumped, int& direction) {
     int num_visits;
 
     // sleep for 1 second, changing sleep duration changes turtle speed
-    sleep(0.1); 
-
+    sleep(0.1);
     switch (state) {
 	case Spawned:
 	    // this is assuming turtle never spawns facing a wall
@@ -151,8 +153,6 @@ Moves studentTurtleStep(bool bumped, int& direction) {
 	    state = Moved; 
 	    return Straight;
 	case Moved:
-    	    tracker.array[tracker.x][tracker.y] += 1;
-    	    mazeVisits(tracker.array[tracker.x][tracker.y]);
 	    num_visits = look_ahead(bumped, direction);
 	    memory[(int)Straight] = num_visits;
             if (num_visits == 0) {
@@ -180,6 +180,7 @@ Moves studentTurtleStep(bool bumped, int& direction) {
 	case Turned_right:
 	    remaining_rights -= 1; 
 	    if (remaining_rights > 0) {
+		state = Turned_right; 
 		return Right; 
 	    }
 	    num_visits = look_ahead(bumped, direction);
@@ -230,7 +231,8 @@ Moves studentTurtleStep(bool bumped, int& direction) {
     }
     
     ROS_INFO("Orientation=%i", direction);
-    return Straight;
+    return Left;
+	    	ROS_INFO("STATE IS LEFT");
 }
 
 
