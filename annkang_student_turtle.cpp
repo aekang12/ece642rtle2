7e0f9ca6c53ecc0c1569e0869d0482e5f8f4b73c
student <aekang12@gmail.com>
2023-11-04 04:54:59 -0400

    simplified turtle code

--- annkang_student_turtle.cpp
+++ annkang_student_turtle.cpp
@@ -22,9 +22,6 @@ static struct Tracker{
     uint8_t x = 11; 
     uint8_t y = 11; 
 } tracker;
-// memory is an array storing number of visits to the cells ahead on the
-// right, left, and straight of the turtle
-static uint8_t memory[3] = {0}; 
 typedef struct coordinates coordinates; 
 struct coordinates {
     uint8_t x;
@@ -34,15 +31,12 @@ struct coordinates {
 // memory value to be 100
 const uint8_t wall_value = 100;
 
-// clear the turtle's memory 
-void clear_memory() {
+// REQUIRES: turtle is going straight 
+// clear the turtle's memory and update tracker 
+void clearmem_updatepos(uint8_t memory[3], int& direction) {
     memory[(uint8_t)Straight] = 0; 
     memory[(uint8_t)Left] = 0; 
     memory[(uint8_t)Right] = 0; 
-}
-
-// update tracker when turtle moves straight
-void update_pos(int& direction) {
     switch ((Directions)direction) {
 	case North:
 	    tracker.y = uint8_t(tracker.y+1);
@@ -64,7 +58,7 @@ void update_pos(int& direction) {
     return; 
 }
 
-// gets coordinates of the cell direclty in front of it 
+// gets coordinates of the cell direclty in front of turtle 
 coordinates get_pos_ahead(int& direction) {
     coordinates c; 
     switch ((Directions)direction) {
@@ -91,35 +85,31 @@ coordinates get_pos_ahead(int& direction) {
     return c;
 }
 
-// updates the turtle's memory of what's direclty in front of it 
-uint8_t look_ahead(bool bumped, int& direction) {
+// return true if turtle should go straight, false otherwise 
+// checks if cell in front of turtle is unvisited 
+uint8_t go_straight(bool bumped, Moves move, int& direction, uint8_t memory[3]) {
     const uint8_t wall_val = (uint8_t)wall_value;
+    uint8_t num_visits = 0;
     if (bumped) {
-	return wall_val; 
+	memory[(uint8_t)move] = wall_val;
+	return false;
     } else {
-	coordinates c = get_pos_ahead(direction); 
-    	return tracker.array[c.x][c.y];
-    }
-}
-
-// check if the turlte is in a dead end
-bool all_walls(uint8_t memory[3]) {
-    uint8_t min_value = wall_value;
-    for (uint8_t i = 0; i < 3; ++i) {
-	if (memory[i] < min_value) {
-	    min_value = memory[i];
+	coordinates c = get_pos_ahead(direction);
+	num_visits = tracker.array[c.x][c.y];
+	if (num_visits ==0) {
+	    clearmem_updatepos(memory, direction);
+	    return true;
+	} else {
+	    coordinates c = get_pos_ahead(direction); 
+	    memory[(uint8_t)move] = tracker.array[c.x][c.y];
+	    return false; 
 	}
     }
-    if (min_value == wall_value) {
-	// dead end, return empty DIRS
-	return true; 
-    }
-    return false;
 }
 
-// get DIRS, a list of directions to turn in to face the least 
+// return how many lefts the turtle should turn to face the least 
 // visited path 
-std::list<Moves> get_DIRS(uint8_t memory[3]) {
+int count_lefts(uint8_t memory[3]) {
     uint8_t min_value = wall_value;
     uint8_t min_index = 0;
     for (uint8_t i = 0; i < 3; ++i) {
@@ -128,101 +118,80 @@ std::list<Moves> get_DIRS(uint8_t memory[3]) {
 	    min_index = i;
 	}
     }
-    if ((Moves)min_index == Right) {
-	return {}; 
+    if (min_value == wall_value) {
+	return 3; 
+    } else if ((Moves)min_index == Right) {
+	return 0; 
     } else if ((Moves)min_index == Left) {
-	return {Left, Left};
+	return 2;
     } else {
-	return {Left};
+	return 1;
     }
 }
 
 // this procedure recommends next move following DFS 
-// bumped is true if there is a wall in front of the turtle, false otherwise
 Moves studentTurtleStep(bool bumped, int& direction) {
     static State state = Moved;
-    static int remaining_rights = 0;
-    static std::list<Moves> DIRS;
-    uint8_t num_visits;
+    static Moves next_move = Straight;
+    static int16_t remaining_rights = -1;
+    static int16_t remaining_lefts = -1;
+    // memory is an array storing number of visits to the cells ahead on the
+    // right, left, and straight of the turtle
+    static uint8_t memory[3] = {0}; 
 
     // sleep for x seconds, changing sleep duration changes turtle speed
     sleep(0.1);
     switch (state) {
 	case Moved:
-	    num_visits = look_ahead(bumped, direction);
-	    memory[(uint8_t)Straight] = num_visits;
-            if (num_visits == 0) {
-		clear_memory();
-	        update_pos(direction);
-		state = Moved;
-		return Straight;
+	    if (go_straight(bumped,Straight, direction, memory)) {
+		next_move = Straight;
 	    } else {
 	        state = Turned_left; 
-		return Left;
+		next_move = Left;
 	    } 
+	    break;
 	case Turned_left:
-	    num_visits = look_ahead(bumped, direction);
-	    memory[(uint8_t)Left] = num_visits;
-            if (num_visits == 0) {
-		clear_memory();
-	        update_pos(direction);
+	    remaining_lefts -= 1; 
+	    if (remaining_lefts > 0) {
+		next_move = Left; 
+	    }
+	    else if (remaining_lefts == 0) {
+	        clearmem_updatepos(memory, direction);
 		state = Moved;
-		return Straight;
+		next_move = Straight;
+	    }
+	    else if (go_straight(bumped,Left, direction, memory)) {
+		state = Moved; 
+		next_move =  Straight;
 	    } else {
 		remaining_rights = 2; 
 		state = Turned_right; 
-		return Right;
+		next_move =  Right;
 	    }
+	    break;
 	case Turned_right:
 	    remaining_rights -= 1; 
 	    if (remaining_rights > 0) {
-		state = Turned_right; 
-		return Right; 
+		next_move = Right; 
 	    }
-	    num_visits = look_ahead(bumped, direction);
-	    memory[(uint8_t)Right] = num_visits;
-            if (num_visits == 0) {
-		clear_memory();
-	        update_pos(direction);
+	    else if (go_straight(bumped,Right, direction, memory)) {
 		state = Moved;
-		return Straight;
-	    } else {
-		if (all_walls(memory)) {
-		    state = Back;
-		    return Right;
-		}
-		DIRS = get_DIRS(memory);
-		if (DIRS.empty()) {
-		    clear_memory();
-	            update_pos(direction);
-		    state = Moved;
-		    return Straight;
-		}
-		state = Turn_min;
-		Moves next = DIRS.front();  
-		DIRS.pop_front();
-		return next;
+		next_move = Straight;
 	    }
-	case Back:
-	    clear_memory();
-	    update_pos(direction);
-	    state = Moved;
-	    return Straight;
-	case Turn_min:
-	    if (DIRS.empty()) {
-	    	clear_memory();
-	    	update_pos(direction);
-	    	state = Moved;
-		return Straight;
+	    else if (count_lefts(memory) == 0) {
+		clearmem_updatepos(memory, direction);
+	  	state = Moved;
+		next_move = Straight;
 	    } else {
-		Moves next = DIRS.front();
-		DIRS.pop_front(); 
-		return next; 
-	    }
+	        remaining_lefts = count_lefts(memory);
+	        state = Turned_left; 
+	        next_move = Left;
+	    } 
+	    break;
 	default:
 	    ROS_ERROR("Invalid direction!"); 
     }
-    return Left;
+    return next_move;
 }
 
 
